d693e5580199ffa3ac20045f7f3d298fb67788d1
al-script <alec.sorensen.as@gmail.com>
2024-09-01 14:12:10 -0700

    Merge project files

--- components/american-to-british-titles.js
+++ components/american-to-british-titles.js
@@ -1,8 +1,18 @@
 module.exports = {
-  'mr.': 'mr',
-  'mrs.': 'mrs',
-  'ms.': 'ms',
-  'mx.': 'mx',
-  'dr.': 'dr',
-  'prof.': 'prof'
-}
+  // simply change to uppercase?
+  "Mr.": "Mr",
+  "Mrs.": "Mrs",
+  "Ms.": "Ms",
+  "Mx.": "Mx",
+  "Dr.": "Dr",
+  "Prof.": "Prof",
+};
+
+// module.exports = {
+//   'mr.': 'mr',
+//   'mrs.': 'mrs',
+//   'ms.': 'ms',
+//   'mx.': 'mx',
+//   'dr.': 'dr',
+//   'prof.': 'prof'
+// }
